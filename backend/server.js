609c3e7605fe077101cb7868afabd6ffeb4939a2
Jacob Baynes <jacob.baynes@icloud.com>
2020-03-05 21:14:01 -0500

    backend edits

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var app = express();
 
-app.get('/', (req, res) => {
-    res.send('hello');
+var messages = [
+    { text: 'some text', owner: 'Tim' },
+    { text: 'other message', owner: 'Jane' },
+    { text: 'newest message', owner: 'Greg' }
+];
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
+
+app.get('/messages', (req, res) => {
+    res.json(messages);
+})
+
+app.post('/message', (req, res) => {
+    console.log(req.body);
 })
 
 app.listen(1234);
